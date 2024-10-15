d04ae37a505e66f43e6a5be05e8bbb96b7d86e21
unknown <matheusmartinsscheffer@hotmail.com>
2024-10-15 19:14:37 -0300

    finalizar api

--- back/Api.py
+++ back/Api.py
@@ -1,6 +1,28 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Declare a variable as a str
+# and get editor support inside the function
+def main(user_id: str):
+    return user_id
+
+
+# A Pydantic model
+class User(BaseModel):
+    nome: str
+    email: str
+    senha: str
+
+class UserLogin(BaseModel):
+    nome: str
+    senha: str
+
+class UserEdit(BaseModel):
+    email: str
+    senha: str
+
 
 app = FastAPI()
 
@@ -12,54 +34,60 @@ class Banco:
 
 
 @app.get("/")
-def read_root():
+def getAll():
     return Banco.banco
 
 
 @app.post("/create")
-def create(user: dict):
-
-    if type(user) != dict:
+def create(user: User):
+    print(type(user))
+    if type(user) != User:
         return {"Message": "O tipo de dado enviado não é um dicionário."}
 
     valid_fields = ["nome", "email", "senha"]
-    for k, v in user.items():
+    for k, v in user.__dict__.items():
         if not (k in valid_fields):
             return {"Message": f'Campo {k} invalido.'}
 
-    if not type(user["nome"]) is str:
+    if not type(user.nome) is str:
         return {"Message": "O Nome deverá ser uma string"}
-    if not type(user["email"]) is str:
+    if not type(user.email) is str:
         return {"Message": "O Email deverá ser uma string"}
-    if not type(user["email"]) is str:
-        return {"Message": "O Email deverá ser uma string"}
+    if not type(user.email) is str:
+        return {"Message": "O senha deverá ser uma string"}
 
     Banco.banco.append({
-        "nome": user["nome"],
-        "email": user["email"],
-        "senha": user["senha"]
+        "nome": user.nome,
+        "email": user.email,
+        "senha": user.senha
     })
 
-    return {"nome": user["nome"], "email": user["email"]}
+    return {"nome":  user.nome, "email": user.email}
 
 
 @app.post("/login")
-def login(nome: str, senha: str):
-    if not type(nome) is str:
+def login(user: UserLogin):
+    valid_fields = ["nome", "senha"]
+    for k, v in user.__dict__.items():
+        if not (k in valid_fields):
+            return {"Message": f'Campo {k} invalido.'}
+        
+    if not type(user.nome) is str:
         return {"Message": "O Nome deverá ser uma string"}
 
     if (len(Banco.banco) == 0):
         return {"Message": "Não há usuários cadastrados"}
 
-    for user in Banco.banco:
-        for k, v in user.items():
-            if user["nome"] == nome and user["senha"] == senha:
-                return {"nome": user["nome"], "senha": user["senha"]}
+    for u in Banco.banco:
+        for k, v in u.items():
+            if u["nome"] == user.nome and u["senha"] == user.senha:
+                print(f'Nome: {u["nome"]} == {user.nome}\nSenha: {u["senha"] == {user.senha}}')
+                return {"nome": u["nome"], "senha": u["senha"]}
         return {"Message": "Usuario nao encontrado."}
 
 
 @app.put("/editar")
-def edit(nome: str, obj: object):
+def edit(nome: str, obj: UserEdit):
     if (type(nome) != str):
         return {"Message": "Nome deve ser uma string."}
 
@@ -72,12 +100,9 @@ def edit(nome: str, obj: object):
     if objIndex == -1:
         return {"Message": f'Usuario {nome} não encontrado.'}
         
-    valid_fields = ["nome", "email", "senha"]
-    for k, v in obj.items():
-        if k in valid_fields:
+    for k, v in obj.__dict__.items():
+        if v:
             Banco.banco[objIndex][k] = v
-        else:
-            return {"Message": f'Campo {k} invalido.'}
 
 
     return {"Message": "Usuario atualizado" , "Resultado": {"nome":Banco.banco[objIndex]["nome"], "email":Banco.banco[objIndex]["email"] }}
@@ -88,7 +113,7 @@ def delete(nome: str):
 
     for i in range(len(Banco.banco)):
         if Banco.banco[i]["nome"] == nome:
-            Banco.banco.pop(i)
+            del Banco.banco[i]
             return {"Message": f'Usuario {nome} deletado com sucesso.'}
 
     return {"Message": f'Usuario {nome} não encontrado.'}
